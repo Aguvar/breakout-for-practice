6b42b1b3651e954b42067ea3ef63338a31a8e18d
Dingo <gustivarela@hotmail.com>
2017-01-11 15:45:40 -0300

    Ball made
    Looks kinda nice, watch out for scale afterwards

--- core/src/com/dingogames/PlayScreen.java
+++ core/src/com/dingogames/PlayScreen.java
@@ -4,8 +4,11 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.*;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
@@ -19,9 +22,11 @@ public class PlayScreen implements Screen{
     private World world;
     private Box2DDebugRenderer renderer;
     private Body ball;
+    private Sprite ballSprite;
 
     //World to simulation conversion
     private final float WtoS = 1/40f;
+    private final float StoW = 40f;
 
     public PlayScreen(BreakoutGame game) {
         this.game = game;
@@ -33,12 +38,15 @@ public class PlayScreen implements Screen{
 
         viewport = new FitViewport(360,640,camera);
 
+        ballSprite = new Sprite(new Texture(Gdx.files.local("breakoutBall.png")));
+        ballSprite.setScale(0.5f);
+
         generatePhysics();
 
     }
 
     private void generatePhysics() {
-        world = new World(new Vector2(1,1), true);
+        world = new World(new Vector2(0,0), true);
 
         //Generate ball
         BodyDef ballDef = new BodyDef();
@@ -56,7 +64,10 @@ public class PlayScreen implements Screen{
 
         ball = world.createBody(ballDef);
         ball.createFixture(circleFix);
-//        ball.setTransform(0,0,0);
+        ball.setUserData(ballSprite);
+
+        //Generate Pallette
+
 
         renderer = new Box2DDebugRenderer(true,false,false,true,false,false);
     }
@@ -70,14 +81,29 @@ public class PlayScreen implements Screen{
     public void render(float delta) {
         Gdx.gl.glClearColor(0.2f, 0, 0.2f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
         world.step(1/60f,6,2);
+
+//        updateSpritePositions();
+
         camera.update();
         game.batch.setProjectionMatrix(camera.combined);
         game.batch.begin();
-
+        drawBodies();
         game.batch.end();
 
         renderer.render(world,camera.combined);
+    }
+
+    private void drawBodies() {
+        Array<Body> holder = new Array<Body>(200);
+        world.getBodies(holder);
+        for (Body body : holder) {
+            if (body.getUserData() instanceof Sprite){
+                ((Sprite) (body.getUserData())).setPosition(body.getPosition().x*StoW - ((Sprite) (body.getUserData())).getWidth()*0.5f,body.getPosition().y*StoW - ((Sprite) (body.getUserData())).getHeight()*0.5f);
+                ((Sprite) (body.getUserData())).draw(game.batch);
+            }
+        }
     }
 
     @Override
