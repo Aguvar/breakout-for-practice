23332f5ee9a211892e79bc408bdf795c94e51b8c
Dingo <gustivarela@hotmail.com>
2017-01-15 21:31:20 -0300

    Started changes on GameoverScreen
    Pull request inminent

--- core/src/com/dingogames/GameoverScreen.java
+++ core/src/com/dingogames/GameoverScreen.java
@@ -3,6 +3,9 @@ package com.dingogames;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 /**
  * Created by Dingo on 09-Jan-17.
@@ -10,11 +13,17 @@ import com.badlogic.gdx.graphics.GL20;
 public class GameoverScreen implements Screen {
 
     private final BreakoutGame game;
-    private int score;
+    private final int score;
+
+    private OrthographicCamera camera;
+    private Viewport viewport;
 
     public GameoverScreen(BreakoutGame game, int score) {
         this.game = game;
         this.score = score;
+
+        camera = new OrthographicCamera();
+        viewport = new FitViewport(360,640, camera);
     }
 
     @Override
@@ -32,7 +41,7 @@ public class GameoverScreen implements Screen {
 
     @Override
     public void resize(int width, int height) {
-
+        viewport.update(width,height);
     }
 
     @Override
